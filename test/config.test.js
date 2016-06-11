49c18243ce6053bf96842cca18c5a14b300a5a48
James Mortemore <jamesmortemore@gmail.com>
2016-06-11 18:18:54 +0100

    Added config test

---
+++ test/config.test.js
@@ -0,0 +1,27 @@
+var assert = require('assert')
+
+describe('Config', function () {
+  beforeEach(function () {
+    var resolved = require.resolve('app/config')
+
+    delete require.cache[resolved]
+  })
+
+  after(function () {
+    process.env.NODE_ENV = 'test'
+  })
+
+  it('should throw an error if incorrect environment', function () {
+    process.env.NODE_ENV = 'nope'
+    assert.throws(function () {
+      require('app/config')
+    }, /No config for environment 'nope'/)
+  })
+
+  it('should throw an error if secret key not set', function () {
+    process.env.NODE_ENV = 'production'
+    assert.throws(function () {
+      require('app/config')
+    }, /You must change the default secretKey/)
+  })
+})
