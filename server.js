fb30ad71b1a00ed71dc66f5eadbda32664e083d7
James Mortemore <jamesmortemore@gmail.com>
2018-03-18 12:34:15 +0000

    Fixed merge conflict

--- server.js
+++ server.js
@@ -1,49 +1,3 @@
-<<<<<<< HEAD
-var pluginable = require('pluginable')
-  , configuration = require('app/config')
-  , bunyanLogger = require('app/logger')(configuration)
-  , loader = require('app/loader')
-  , pm = pluginable(require('./modules'))
-  , moduleCount = 0
-
-pm.on('afterLoad', function (module) {
-  bunyanLogger.info(module.name, 'module loaded')
-  moduleCount++
-})
-
-pm.registerBeforeLoad(function logger(cb) { cb(null, bunyanLogger) })
-pm.registerBeforeLoad(function config(cb) { cb(null, configuration) })
-pm.registerBeforeLoad(function pluginManager(cb) { cb(null, pm) })
-
-pm.load(function (error, modules) {
-  if (error) {
-    bunyanLogger.error(error)
-    process.exit(1)
-  }
-
-  loader(modules, function (error, app) {
-    if (error) {
-      bunyanLogger.error(error)
-      process.exit(1)
-    }
-
-    modules.logger.info(moduleCount + ' modules loaded')
-
-    app.start(function (error) {
-      if (error) {
-        modules.logger.error(error)
-        process.exit(1)
-      }
-
-      require('blocked')(function (ms) {
-        modules.logger.warn('blocked', { ms: ms })
-      })
-
-      modules.logger.info('Server running at:', app.info.uri)
-    })
-  })
-})
-=======
 global.Promise = require('bluebird')
 require('dotenv').config()
 
@@ -79,4 +33,3 @@ const dbConfig =
     process.exit(1)
   }
 })()
->>>>>>> 386a8b5... Rewrite using GraphQL
