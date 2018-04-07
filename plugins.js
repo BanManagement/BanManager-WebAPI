09ea86e4ad69055de6686ef8d37671f01d5c5d50
James Mortemore <jamesmortemore@gmail.com>
2018-04-07 22:22:05 +0100

    Removed legacy file

--- plugins.js
+++
@@ -1,6 +0,0 @@
-module.exports =
-[ 'app/modules/servers/plugins/server-lookup.js'
-, 'app/modules/players/plugins/player-lookup.js'
-, 'app/modules/players/plugins/actor-lookup.js'
-, 'app/lib/boom-json-api.js'
-]
