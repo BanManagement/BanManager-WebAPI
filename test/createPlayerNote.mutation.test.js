6013d8ea1238b9522d00e85cb78a46390e756a82
James Mortemore <jamesmortemore@gmail.com>
2018-04-08 10:42:21 +0100

    Fixed create player note
    Covered with test

---
+++ test/createPlayerNote.mutation.test.js
@@ -0,0 +1,97 @@
+const assert = require('assert')
+const { unparse } = require('uuid-parse')
+const supertest = require('supertest')
+const createApp = require('../app')
+const { createSetup, getAuthPassword } = require('./lib')
+const {
+  createPlayer,
+  createNote
+} = require('./fixtures')
+const { insert } = require('../data/udify')
+
+describe('Mutation create player note', function () {
+  let setup
+  let server
+  let request
+
+  before(async function () {
+    setup = await createSetup()
+    const app = await createApp(setup.dbPool, setup.logger, setup.serversPool)
+
+    server = app.listen()
+    request = supertest(server)
+  })
+
+  after(async function () {
+    await setup.teardown()
+    await server.close() // @TODO Should allow mocha to exit, but it's still hanging :S
+  })
+
+  it('should error if unauthenticated', async function () {
+    const player = createPlayer()
+    const { body, statusCode } = await request
+      .post('/graphql')
+      .set('Accept', 'application/json')
+      .send({ query: `mutation createPlayerNote {
+        createPlayerNote(input: {
+          player: "${unparse(player.id)}", message: "test", server: "${server.id}"
+        }) {
+          id
+        }
+      }`})
+
+    assert.equal(statusCode, 200)
+
+    assert(body)
+    assert.strictEqual(body.errors[0].message,
+      'You do not have permission to perform this action, please contact your server administrator')
+  })
+
+  it('should resolve all fields', async function () {
+    const { config: server, pool } = setup.serversPool.values().next().value
+    const cookie = await getAuthPassword(request, 'admin@banmanagement.com')
+
+    const player = createPlayer()
+    const actor = createPlayer()
+    const note = createNote(player, actor)
+
+    await insert(pool, 'bm_players', [ player, actor ])
+
+    const { body, statusCode } = await request
+      .post('/graphql')
+      .set('Cookie', cookie)
+      .set('Accept', 'application/json')
+      .send({ query: `mutation createPlayerNote {
+        createPlayerNote(input: {
+          player: "${unparse(player.id)}", message: "${note.message}", server: "${server.id}"
+        }) {
+          id
+          message
+          created
+          player {
+            id
+            name
+          }
+          actor {
+            id
+            name
+          }
+          acl {
+            delete
+            update
+            yours
+          }
+        }
+      }`})
+
+    assert.equal(statusCode, 200)
+
+    assert(body)
+    assert(body.data)
+
+    assert.strictEqual(body.data.createPlayerNote.id, '1')
+    assert.strictEqual(body.data.createPlayerNote.message, note.message)
+    assert.deepStrictEqual(body.data.createPlayerNote.acl, { delete: true, update: true, yours: false })
+  })
+
+})
