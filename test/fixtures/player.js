1c83e00200466786e6f361d555c50338f911b527
James Mortemore <jamesmortemore@gmail.com>
2019-03-31 14:33:46 +0100

    fix: avoid duplicate UUIDs

--- test/fixtures/player.js
+++ test/fixtures/player.js
@@ -1,10 +1,11 @@
 const { date, name, internet, random } = require('faker')
 const { parse } = require('uuid-parse')
 const { toLong } = require('ip')
+const generateUUID = require('uuid/v4')
 
 module.exports = function () {
   return {
-    id: parse(random.uuid(), new Buffer(16))
+    id: parse(generateUUID(), new Buffer(16))
   , name: name.firstName()
   , ip: toLong(internet.ip())
   , lastSeen: Math.round((new Date(date.past()).getTime() / 1000))
