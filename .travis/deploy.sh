516e7f728632400c288cceb5b67cbb9eca258853
James Mortemore <jamesmortemore@gmail.com>
2019-04-14 07:29:40 +0100

    feat: deploy to demo automatically

---
+++ .travis/deploy.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+- openssl aes-256-cbc -K $encrypted_651df60c2689_key -iv $encrypted_651df60c2689_iv -in deploy.key.enc -out .travis/deploy.key -d
+chmod +x ./.travis/deploy.sh
+eval "$(ssh-agent -s)"
+chmod 600 .travis/deploy.key
+ssh-add .travis/deploy.key
+ssh-keyscan $DEPLOY_SERVER >> ~/.ssh/known_hosts
+git remote add deploy dokku@$DEPLOY_SERVER:api.demo.banmanagement.com >/dev/null 2>&1
+git config --global push.default simple
+git stash --all
+git checkout master
+git push -f deploy master >/dev/null 2>&1
